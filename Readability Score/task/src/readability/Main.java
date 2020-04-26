85dd510392cf0140384596d56818b146c6ef487e
Nick <mimoun136@yandex.ru>
2020-04-26 12:07:33 +0900

    Complete stage 2.

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        String inputString = scanner.nextLine();
-        if (inputString.length() > 100) {
+        String[] inputString = scanner.nextLine().replaceAll(",","").split("[.!?]".trim());
+        int countWordsInSentence = 0;
+        for (String str: inputString) {
+            String[] temp = str.split("\\b\\s|\\b\u00a0");
+            countWordsInSentence += temp.length;
+        }
+        if ((double)countWordsInSentence / inputString.length > 10.0) {
             System.out.println("HARD");
         }
         else {
