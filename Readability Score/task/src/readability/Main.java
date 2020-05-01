70dec5893edf9748917744cf742c36bacadff8bd
Nick <mimoun136@yandex.ru>
2020-05-01 21:13:48 +0900

    Start working on stage 4.

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -2,7 +2,7 @@ package readability;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
+import eu.crydee.syllablecounter.SyllableCounter;
 import java.util.Scanner;
 
 public class Main {
@@ -17,9 +17,64 @@ public class Main {
         }
         return result.toString();
     }
+    public static void printResult(int choose) {
+        switch (choose) {
+            case 1:
+                System.out.println("This text should be understood by 6 year olds.");
+                break;
+            case 2:
+                System.out.println("This text should be understood by 7 year olds.");
+                break;
+            case 3:
+                System.out.println("This text should be understood by 9 year olds.");
+                break;
+            case 4:
+                System.out.println("This text should be understood by 10 year olds.");
+                break;
+            case 5:
+                System.out.println("This text should be understood by 11 year olds.");
+                break;
+            case 6:
+                System.out.println("This text should be understood by 12 year olds.");
+                break;
+            case 7:
+                System.out.println("This text should be understood by 13 year olds.");
+                break;
+            case 8:
+                System.out.println("This text should be understood by 14 year olds.");
+                break;
+            case 9:
+                System.out.println("This text should be understood by 15 year olds.");
+                break;
+            case 10:
+                System.out.println("This text should be understood by 16 year olds.");
+                break;
+            case 11:
+                System.out.println("This text should be understood by 17 year olds.");
+                break;
+            case 12:
+                System.out.println("This text should be understood by 18 year olds.");
+                break;
+            case 13:
+                System.out.println("This text should be understood by 24 year olds.");
+                break;
+            case 14:
+                System.out.println("This text should be understood by 24+ year olds.");
+                break;
+            default:
+                System.out.println("You're too old person or you don't exist:)");
+                break;
+        }
+    }
     public static void main(String[] args) {
-        String inputPath = args[0];
-        String inputString = load(inputPath);
+        SyllableCounter syllableCounter = new SyllableCounter();
+        /*String inputPath = "";
+        for (int i = 0; i < args.length;  i++) {
+            inputPath = args[0];
+        }*/
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();//load(inputPath);
+        int countSyllable = syllableCounter.count(inputString);
         char[] charInputString = inputString.toCharArray();
         int countChar = 0;
         for (char c : charInputString) {
@@ -31,8 +86,6 @@ public class Main {
         int countWordsInSentence = 0;
         for (String str: arrayInputString) {
             String[] temp = str.split("\\b\\s|\\b\u00a0");
-            System.out.println(Arrays.toString(temp));
-            System.out.println(temp.length);
             countWordsInSentence += temp.length;
         }
         double score = 4.71 * ((double) countChar/countWordsInSentence) + 0.5 * ((double)countWordsInSentence/arrayInputString.length) - 21.43;
@@ -41,54 +94,9 @@ public class Main {
         System.out.println("Words: " + countWordsInSentence);
         System.out.println("Sentences: " + arrayInputString.length);
         System.out.println("Characters: " + countChar);
+        System.out.println("Syllables: " + countSyllable);
         System.out.printf("The score is: %.2f\n", score);
         int choose = (int) Math.ceil(score);
-        switch (choose) {
-            case 1:
-                System.out.println("This text should be understood by 5-6 year olds.");
-                break;
-            case 2:
-                System.out.println("This text should be understood by 6-7 year olds.");
-                break;
-            case 3:
-                System.out.println("This text should be understood by 7-9 year olds.");
-                break;
-            case 4:
-                System.out.println("This text should be understood by 9-10 year olds.");
-                break;
-            case 5:
-                System.out.println("This text should be understood by 10-11 year olds.");
-                break;
-            case 6:
-                System.out.println("This text should be understood by 11-12 year olds.");
-                break;
-            case 7:
-                System.out.println("This text should be understood by 12-13 year olds.");
-                break;
-            case 8:
-                System.out.println("This text should be understood by 13-14 year olds.");
-                break;
-            case 9:
-                System.out.println("This text should be understood by 14-15 year olds.");
-                break;
-            case 10:
-                System.out.println("This text should be understood by 15-16 year olds.");
-                break;
-            case 11:
-                System.out.println("This text should be understood by 16-17 year olds.");
-                break;
-            case 12:
-                System.out.println("This text should be understood by 17-18 year olds.");
-                break;
-            case 13:
-                System.out.println("This text should be understood by 18-24 year olds.");
-                break;
-            case 14:
-                System.out.println("This text should be understood by 24+ year olds.");
-                break;
-            default:
-                System.out.println("You're too old person or you don't exist:)");
-                break;
-        }
+        printResult(choose);
      }
 }
